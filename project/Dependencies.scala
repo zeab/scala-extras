737c9280992d2fdc738b317b80a1535e9b0e1954
kevin downs <pyrosrppltoo@gmail.com>
2019-06-03 08:51:59 -0700

    1.1.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,18 +7,26 @@ object Dependencies {
   val V = new {
     val scalaReflect                = "2.12.6"
     val scalaTest                   = "3.0.5"
+    val logbackJson                 = "5.2"
+    val logback                     = "1.2.3"
   }
 
   //List of Dependencies
   val D = new {
+    //Reflection
     val reflect                     = "org.scala-lang" % "scala-reflect" % "2.12.6"
     //Test
     val scalaTest                   = "org.scalatest" %% "scalatest" % V.scalaTest
+    //Logging
+    val logback                     = "ch.qos.logback" % "logback-classic" % V.logback
+    val logbackJson                 = "net.logstash.logback" % "logstash-logback-encoder" % V.logbackJson
   }
 
   val rootDependencies: Seq[ModuleID] = Seq(
     D.reflect,
-    D.scalaTest
+    D.scalaTest,
+    D.logback,
+    D.logbackJson
   )
 
 }
